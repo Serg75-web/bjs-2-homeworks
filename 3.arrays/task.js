3aa64d90257588bc95bcc953cbd82b1c2c20cac6
Serg75 <activequest@gmail.com>
2026-06-02 13:51:39 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,26 @@
 function compareArrays(arr1, arr2) {
   
+// Сравниваем массивы по длине
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    
+    // Сравниваем элементы на одинаковых индексах с помощью метода every
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
   
+// Отфильтровываем юзеров по полу
+  const filteredUsers = users.filter(user => user.gender === gender);
+  
+  // Если нет пользователей указанного пола, возвращаем 0
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+  
+  // Получение массива возрастов и вычисление среднего значения
+  const averageAge = filteredUsers.map(user => user.age).reduce((sum, age) => sum + age, 0) / filteredUsers.length;
+
+  return averageAge;
 }
